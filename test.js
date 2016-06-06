da68f7ec9f97d09ee464ec08b18a91ebe305ca0e
Manan V. Panchal <mananvpanchal@gmail.com>
2016-06-06 11:35:24 +0530

    added babelrc

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-//var testModule = require('test-module');
-//console.log(testModule);
-//console.log(testModule.helloMsg());
-//require('babel/register')({
-//  "presets": ["es2015"]
-//})
-require('babel-register')({
-	"presets": ["es2015"]
-});
 import { add, multiply, minus, divide } from 'es6-module';
 
 console.log(add(3, 6));
